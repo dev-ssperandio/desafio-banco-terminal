ba4b738ca6da8e788b50c0787223f8efccdf58a3
ssperandio <dev.ssperandio@gmail.com>
2024-08-15 12:24:48 -0300

    Deixando mais evidente a utilização

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,7 +11,7 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
-        System.out.println("Olá, digite seu primeiro nome e último nome? ");
+        System.out.println("Olá, digite seu primeiro nome, pressione a tecla 'Enter' e na segunda linha digite o último nome? ");
         String nomeCliente = scanner.next().concat(" ").concat(scanner.next());
 
         System.out.println("Por favor, digite o número da agência: ");
@@ -20,8 +20,8 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o número da conta: ");
         String numero = scanner.next();
 
-        System.out.println("Por favor, digite o saldo: ");
-        double saldo = scanner.nextDouble();
+        System.out.print("Por favor, digite o saldo: \nR$ ");
+        double saldo = Double.parseDouble(scanner.next());
 
         System.out.println("Olá ".concat(nomeCliente).concat(", obrigado por criar uma conta em nosso banco, sua agência é ").concat(agencia).concat(", conta ")+numero+" e seu saldo de R$ "+saldo+" já está disponível para saque.");
     }
